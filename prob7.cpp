0c1d5313384d8844d4bfb5ab10a31f96514712cf
pfvilcav <pfvilcav@unjbg.edu.pe>
2025-05-31 22:12:32 -0500

    Problema #7: Se implementaron las restricciones: 'm' mayor que cero y 'n'.

--- prob7.cpp
+++ prob7.cpp
@@ -13,8 +13,17 @@ int main() {
     int m,n;
     cout<<"Combinatoria de:"<<endl;
     cin>>m;
+    if (m<=0) {
+        cout<<"El numero debe ser mayor que cero...";
+        return 0;
+    }
     cout<<"en:"<<endl;
     cin>>n;
-    cout<<factorial(m)/(factorial(n)*factorial(m-n));
+    if (m>n) {
+        cout<<"es igual a:"<<endl<<factorial(m)/(factorial(n)*factorial(m-n));
+    }
+    else {
+        cout<<"El primer numero debe ser mayor que el segundo...";
+    }
     return 0;
 }
