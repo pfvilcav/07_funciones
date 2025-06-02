a61ed355e3dd7eaddaab57fc9bac26ad597911bd
pfvilcav <pfvilcav@unjbg.edu.pe>
2025-06-01 22:06:24 -0500

    Problema #4(Saber si un numero es perfecto)

---
+++ prob4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int esPerfecto(int n) {
+    int resul=0;
+    for (int i=2; i<=n; i++) {
+        if (n%i==0) {
+            resul=resul+(n/i);
+        }
+    }
+    
+    return resul;
+}
+
+int main() {
+    int NUM;
+    cout<<"Ingrese el numero a verificar si es perfecto:"<<endl;
+    cin>>NUM;
+    if (esPerfecto(NUM)==NUM) {
+        cout<<"El numero es perfecto.";
+    }
+    else {
+        cout<<"El numero no es perfecto.";
+    }
+    return 0;
+}
