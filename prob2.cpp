72596725059e14e0758d84f8884c545c80be009a
pfvilcav <pfvilcav@unjbg.edu.pe>
2025-06-02 23:41:48 -0500

    Problema #2: El programa ahora prohibe los valores menores o iguales a cero.

--- prob2.cpp
+++ prob2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int division(int dividendo, int divisor) {
+int divYres(int dividendo, int divisor) {
     int cociente=0, res;
     for (int i=1; dividendo-divisor>=0; i++) {
         res=dividendo-divisor;
@@ -17,15 +17,23 @@ int main() {
     int NUM1, NUM2, MAYOR;
     cout<<"Ingrese un numero: ";
     cin>>NUM1;
+    if (NUM1<=0) {
+        cout<<"El valor debe ser mayor que cero...";
+        return 0;
+    }
     cout<<"Ingrese otro numero: ";
     cin>>NUM2;
+    if (NUM2<=0) {
+        cout<<"El valor debe ser mayor que cero...";
+        return 0;
+    }
     if( NUM1>NUM2) {
         MAYOR=NUM1;
     }
     else {
         MAYOR=NUM2;
     }
-    cout<<"El residuo y el cociente respectivamente son los siguientes: "<<endl;
-    cout<<division(MAYOR, NUM2);
+    cout<<"El residuo y el cociente, respectivamente, son los siguientes: "<<endl;
+    cout<<divYres(MAYOR, NUM2);
     return 0;
 }
